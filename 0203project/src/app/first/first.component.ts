d368ad7bdcaafcd6ce4013435177105a81113a31
lee98433 <lee98433@gmail.com>
2026-02-03 10:31:22 +0800

    no message

--- 0203project/src/app/first/first.component.ts
+++ 0203project/src/app/first/first.component.ts
@@ -1,11 +1,31 @@
+import { ExampleService } from './../@service/example.service';
+import { Router } from '@angular/router';
+import { routes } from './../app.routes';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-first',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './first.component.html',
   styleUrl: './first.component.scss'
 })
 export class FirstComponent {
+  constructor(
+    private router:Router,
+    private exampleService:ExampleService
+  ){}
+
+
+  account!:string;//帳號
+  password!:string//密碼
+
+  sendData(){
+    this.exampleService.globalData={
+      account:this.account,
+      password:this.password,
+    }
+    this.router.navigate(['/second'])
+  }
 
 }
